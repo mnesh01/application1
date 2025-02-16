7868398a8ca7854c4c603cb7a27410e26dc2de2a
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2025-02-16 23:20:49 +0300

    fix router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import JobApplicationForm from './JobApplicationForm';
+import SuccessPage from "./Success";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <JobApplicationForm />
-    </div>
+    <Router>  {}
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<JobApplicationForm />} />
+          <Route path="/success" element={<SuccessPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
